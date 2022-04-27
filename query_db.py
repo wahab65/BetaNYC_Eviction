06a2a69708f570aced3d4c326eea9987782e140e
zhik <zhikhe@gmail.com>
2022-04-27 14:24:37 -0400

    status message and checks

--- query_db.py
+++ query_db.py
@@ -10,9 +10,11 @@ load_dotenv()
 engine = sqlalchemy.create_engine(os.getenv('NYCDB_DATABASE'))
 
 for sql_file_name in glob.glob('*.sql'):
+    print(f'Running {sql_file_name}')
     with open(sql_file_name, 'r') as file:
         query = file.read()
 
     df = pd.read_sql_query(query, con=engine)
     csv_file_name = sql_file_name.replace('.sql','.csv')
     df.to_csv(csv_file_name, index = False)
+    print(f'Saved to {csv_file_name}\n')
